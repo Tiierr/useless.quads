60cbcf7148e5f7a053da6f1648173249b9a289ec
tiierr <shiliuhuasheng@gmail.com>
2020-06-15 20:59:33 +0800

    feat(init): Init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useRef} from 'react';
+import * as d3 from 'd3';
+
+import Quad from './tools/quad';
+import Circle from './tools/circle';
+import {findImage, getRandom} from "./tools/common";
+
 
 function App() {
+
+  const images: Array<string> = findImage();
+
+  const dotRef = useRef(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const width = 1024;
+  const height = 1024;
+
+  // todo: support upload picture
+  // const [quadImage] = useState<string>(getRandom(images));
+
+  const quadImage = getRandom(images);
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    // @ts-ignore
+    let context: CanvasRenderingContext2D = canvasRef.current.getContext('2d');
+
+    let img = new Image();
+    img.onload = function(){
+      context.drawImage(img, 0, 0, height, width);
+      const svg = d3.select(dotRef.current)
+          .append("svg")
+          .attr("viewBox", `0 0 ${height} ${width}`)
+          .style("display", "block");
+
+      const quad = new Quad(0, 0, width, width, null, context);
+      if (quad.e > 30) {
+        document.body.style.backgroundColor = "#FFFFFB";
+      }
+
+      Circle(svg, [quad], true);
+    };
+    img.src = quadImage;
+  }, [quadImage]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center">
+      <div className="cont">
+        <div className="dot" ref={dotRef}>
+          <canvas height={height} width={width} ref={canvasRef} style={{display: "none"}}/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/tools/calculate.tsx
@@ -0,0 +1,55 @@
+function colorFromHistogram(histogram: Uint32Array) {
+    const [r, re] = weightedAverage(histogram.subarray(0, 256));
+    const [g, ge] = weightedAverage(histogram.subarray(256, 512));
+    const [b, be] = weightedAverage(histogram.subarray(512, 768));
+    return [
+        Math.round(r),
+        Math.round(g),
+        Math.round(b),
+        re * 0.2989 + ge * 0.5870 + be * 0.1140
+    ];
+}
+
+function weightedAverage(histogram: Uint32Array) {
+    // 加权平均值 weighted average value
+    let total = 0;
+    let value = 0;
+    for (let i = 0; i < 256; ++i) {
+        total += histogram[i];
+        value += histogram[i] * i;
+    }
+    value /= total;
+
+    // 加权标准差 WEIGHTED STANDARD DEVIATION
+    let error = 0;
+    for (let i = 0; i < 256; ++i) error += (value - i) ** 2 * histogram[i];
+    return [value, Math.sqrt(error / total)];
+}
+
+function computeHistogram(imageContext: CanvasRenderingContext2D, x: number, y: number, w: number, h: number) {
+    const {data} = imageContext.getImageData(x, y, w, h);
+    const histogram = new Uint32Array(1024);
+    // 将 RGBA 数组转化为直方图
+    // R: 0 - 256
+    // G: 256 - 512
+    // B: 512 - 768
+    // A: 768 - 1024
+    for (let i = 0, n = data.length; i < n; i += 4) {
+        ++histogram[0 * 256 + data[i + 0]];
+        ++histogram[1 * 256 + data[i + 1]];
+        ++histogram[2 * 256 + data[i + 2]];
+        ++histogram[3 * 256 + data[i + 3]];
+    }
+    return histogram;
+}
+
+function checkSplit(args: Array<number>): boolean {
+    return args.filter(x => x >= 1).length === args.length;
+}
+
+
+export {
+    colorFromHistogram,
+    computeHistogram,
+    checkSplit
+}
