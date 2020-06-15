679aa44dc76c2f1857c8986cbd22ddf06f0e0db5
tiierr <shiliuhuasheng@gmail.com>
2020-06-15 21:30:49 +0800

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# Useless Quads
-
-The program target an random image, transform it to a circle. 
-
-Each circle will split into four quadrants when user activate it.
-
-
---
+++ precache-manifest.e89510165a702733e7abcb29e772895e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "637a2f0d961d85405f2a9f9f08111bf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "395636f0df996fc3a433",
+    "url": "/static/css/main.b439301d.chunk.css"
+  },
+  {
+    "revision": "74a585b7c1b11e9ff916",
+    "url": "/static/js/2.8539c99c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8539c99c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "395636f0df996fc3a433",
+    "url": "/static/js/main.f76f8f1a.chunk.js"
+  },
+  {
+    "revision": "d4a77cd77cd9346e120f",
+    "url": "/static/js/runtime-main.d47f61b1.js"
+  },
+  {
+    "revision": "ddc13a2d2995f3465747b936fe477ce4",
+    "url": "/static/media/p1.ddc13a2d.jpg"
+  },
+  {
+    "revision": "5dc7d115af1b4c97dd6040b97de88b07",
+    "url": "/static/media/p3.5dc7d115.png"
+  }
+]);
